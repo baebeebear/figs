278ef20052d6d71987d4b1e9e1ffbc324429d63f
baebeebear <bearrys393@gmail.com>
2026-08-12 19:35:09 -0400

    add capacitor ios project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.figsapp.figs',
+  appName: 'figs',
+  webDir: 'dist'
+};
+
+export default config;
